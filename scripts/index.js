88fee798bdcf5d4ebd74037409e6393961903f6f
Igor Varekhov <darisvet.sokolov@yandex.ru>
2024-09-28 23:04:46 +0300

    Update deploy

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,10 @@
-$(document).ready(function() {
-  $('.slider').slick({
-    arrows: false,
-    dots: true,
-  });
+$(window).on("load resize", () => {
+  if ($(window).width() < 767) {
+    $(".slider:not(.slick-initialized)").slick({
+      dots: true,
+      arrows: false,
+    });
+  } else {
+    $(".slider.slick-initialized").slick("unslick");
+  }
 });
