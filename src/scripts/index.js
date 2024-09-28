c8a3dcd97a493b31ba043eafbbf33c541b5010bf
garryvar <darisvet.sokolov@yandex.ru>
2024-09-28 22:22:45 +0300

    Added desktop responsive

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,10 @@
-$(document).ready(function () {
-  $('.slider').slick({
-    arrows: false,
-    dots: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: "unslick",
-        dots: false,
-      }
-    ]
-  });
+$(window).on("load resize", () => {
+  if ($(window).width() < 767) {
+    $(".slider:not(.slick-initialized)").slick({
+      dots: true,
+      arrows: false,
+    });
+  } else {
+    $(".slider.slick-initialized").slick("unslick");
+  }
 });
-
