19004ea11a0ecfe9bb2fe8573702172abe35b315
Igor Varekhov <darisvet.sokolov@yandex.ru>
2024-09-29 11:35:20 +0300

    Update deploy

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,43 @@ $(window).on("load resize", () => {
     $(".slider.slick-initialized").slick("unslick");
   }
 });
+
+let minute = 30;
+let sec = 60;
+
+const timer = () => {
+  const hoursDigit = document.querySelector(".timer__hours");
+  const minutesDigit = document.querySelector(".timer__minutes");
+  const secDigit = document.querySelector(".timer__seconds");
+
+  sec--;
+  secDigit.textContent = sec;
+
+  if (sec < 10) {
+    secDigit.textContent = `0${sec}`;
+
+    if (sec <= 0) {
+      minute--;
+      sec = 60;
+      secDigit.textContent = sec;
+      minutesDigit.textContent = minute;
+
+      if (minute < 10) {
+        minutesDigit.textContent = `0${minute}`;
+
+        if (minute <= 0) {
+          minute = "00";
+          minutesDigit.textContent = minute;
+
+          sec = "00";
+          secDigit.textContent = sec;
+        }
+      }
+    }
+  }
+};
+
+setInterval(timer, 1000);
+clearInterval(timer);
+
+window.addEventListener("load", timer);
